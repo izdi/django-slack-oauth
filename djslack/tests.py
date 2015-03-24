87e1ae0536d657433d11a052677e3931acc353ed
Sergey Keller <s.keller@itransition.com>
2015-03-24 20:29:23 +0300

    module setup tests

--- djslack/tests.py
+++ djslack/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.conf import settings
 
-# Create your tests here.
+
+class TestModuleSetup(TestCase):
+    slack_settings = [
+        'SLACK_CLIENT_ID',
+        'SLACK_CLIENT_SECRET',
+        'SLACK_AUTHORIZATION_URL',
+        'SLACK_OAUTH_ACCESS'
+    ]
+
+    def test_settings(self):
+        for setting_var in self.slack_settings:
+            if not hasattr(settings, setting_var):
+                raise self.fail('Set up %s in your project settings' % setting_var)
+
+    def test_slack_request(self):
+        pass
