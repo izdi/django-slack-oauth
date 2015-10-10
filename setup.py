499bf5e2afb3a8b40d206280aa1cb0dba90fff21
izdi <izdieu@gmail.com>
2015-10-10 13:56:08 +0300

    Version fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-slack-oauth',
-    version='0.4',
+    version='0.4.1',
     packages=['django_slack_oauth',
               'django_slack_oauth.migrations',
               'django_slack_oauth.south_migrations'],
