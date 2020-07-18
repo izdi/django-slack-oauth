b15d0413b31af80f885f7fffd93553eda47f4459
Stavros Korokithakis <hi@stavros.io>
2020-07-18 17:44:37 +0300

    Fix python_2_unicode_compatible import error (closes #26)

--- django_slack_oauth/models.py
+++ django_slack_oauth/models.py
@@ -4,7 +4,7 @@ from jsonfield import JSONField
 
 from django.conf import settings
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 
 
 __all__ = (
